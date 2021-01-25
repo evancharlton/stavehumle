d2985196ebe6a07bf457bf2a750f66d9024f9c37
Evan Charlton <evancharlton@microsoft.com>
2021-01-25 20:47:23 +0100

    Add storybook

---
+++ src/Hive/Hive.stories.tsx
@@ -0,0 +1,10 @@
+import Hive from './Hive';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Hive',
+};
+
+export const Base = () => {
+  return <Hive />;
+};
