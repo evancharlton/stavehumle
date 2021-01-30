d5d84f4f1cac30bbfc62701a2e39997cdb8d2da3
Evan Charlton <evancharlton@microsoft.com>
2021-01-30 16:52:40 +0100

    Get rid of the start page
    Just send 'em to today's puzzle.

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -1,34 +1,26 @@
-import { ChangeEvent, useCallback, useState } from 'react';
+import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+
+const twoDigits = (v: number) => {
+  if (v < 10) {
+    return `0${v}`;
+  }
+  return v;
+};
 
 const Start = () => {
   const history = useHistory();
-  const [value, setValue] = useState('');
 
-  const onChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      setValue(e.target.value);
-    },
-    [setValue]
-  );
+  useEffect(() => {
+    const now = new Date();
+    history.replace(
+      `/${[now.getFullYear(), now.getMonth() + 1, now.getDate()]
+        .map(twoDigits)
+        .join('-')}`
+    );
+  }, [history]);
 
-  const onSubmit = useCallback(
-    (e: React.FormEvent) => {
-      e.preventDefault();
-      history.push(`/${value}`);
-    },
-    [value, history]
-  );
-
-  return (
-    <div>
-      <h1>Choose a date</h1>
-      <form onSubmit={onSubmit}>
-        <input type="text" onChange={onChange} value={value} />
-        <button type="submit">Go</button>
-      </form>
-    </div>
-  );
+  return null;
 };
 
 export default Start;
