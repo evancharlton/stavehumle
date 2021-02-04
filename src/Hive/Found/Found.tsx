4c6cd99259c9c1f046dcbad24a106358f8fa664c
Evan Charlton <evancharlton@microsoft.com>
2021-02-04 21:08:39 +0100

    Add an ordbank explanation

--- src/Hive/Found/Found.tsx
+++ src/Hive/Found/Found.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import { useGame } from '../hooks';
 import classes from './Found.module.css';
+import WordsInfo from './WordsInfo';
 
 const Found = () => {
   const { found: words } = useGame();
@@ -10,19 +11,23 @@ const Found = () => {
     );
     return foo;
   }, [words]);
+
   return (
-    <div className={classes.wordList}>
-      {cleaned.map((word) => (
-        <a
-          className={classes.word}
-          key={word}
-          href={`https://naob.no/s%C3%B8k/${encodeURIComponent(word)}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {word}
-        </a>
-      ))}
+    <div className={classes.container}>
+      <div className={classes.wordList}>
+        {cleaned.map((word) => (
+          <a
+            className={classes.word}
+            key={word}
+            href={`https://naob.no/s%C3%B8k/${encodeURIComponent(word)}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {word}
+          </a>
+        ))}
+      </div>
+      <WordsInfo />
     </div>
   );
 };
---
+++ src/Hive/Found/WordsInfo/WordsInfo.tsx
@@ -0,0 +1,51 @@
+import classes from './WordsInfo.module.css';
+import { useMemo, useState } from 'react';
+import { MdHelpOutline as HelpIcon } from 'react-icons/md';
+import Modal from '../../../Modal';
+
+const WordsInfo = () => {
+  const [showing, setShowing] = useState(false);
+
+  const modal = useMemo(() => {
+    if (!showing) {
+      return false;
+    }
+    return (
+      <Modal title="«Hva er disse ordene?»" onClose={() => setShowing(false)}>
+        <p>
+          Ordene som brukes her kommer fra den{' '}
+          <a
+            href="https://www.nb.no/sprakbanken/ressurskatalog/oai-nb-no-sbr-5/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Norsk ordbank
+          </a>
+          . Alle ordene stemmer, men noen ord blir sjelden (eller aldri) sett i
+          det virkelige liv.
+        </p>
+        <p>
+          <a
+            href="mailto:evancharlton+humle@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Skriv en e-post
+          </a>{' '}
+          hvis du finner et galt ord 😀
+        </p>
+      </Modal>
+    );
+  }, [showing]);
+
+  return (
+    <>
+      <button onClick={() => setShowing(true)} className={classes.button}>
+        <HelpIcon />
+      </button>
+      {modal}
+    </>
+  );
+};
+
+export default WordsInfo;
