eeb8845856a8672bf8cf5efa8188c3f3264ea443
Evan Charlton <evancharlton@microsoft.com>
2021-02-08 22:25:14 +0100

    Add a fancy new icon
    Give the app a proper logo, courtesy of Bahar!

--- src/Hive/Header/Header.tsx
+++ src/Hive/Header/Header.tsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom';
 import InfoButton from './InfoButton';
 import CalendarButton from './CalendarButton';
 import classes from './Header.module.css';
+import Icon from 'Humle.svg';
 
 const Header = () => {
   return (
     <div className={classes.header}>
       <h1>
-        <Link to="/">Staver humle 🐝</Link>
+        <Link to="/">Staver humle</Link>
+        <img src={Icon} alt="Staver humle icon" />
       </h1>
       <CalendarButton />
       <InfoButton />
