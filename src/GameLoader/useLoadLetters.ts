986903dd93cdfb14809eadb84947401a15443e39
Evan Charlton <evancharlton@gmail.com>
2024-08-18 12:47:19 +0200

    Attempt to fix the JSON loading error

--- src/GameLoader/useLoadLetters.ts
+++ src/GameLoader/useLoadLetters.ts
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
-import { useSetRecoilState } from "recoil";
-import { gzipJsonFetch } from "../api";
-import { useGameId } from "../hooks";
-import { gamePuzzleId } from "./recoil";
+import { useEffect, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { gzipJsonFetch } from '../api';
+import { useGameId } from '../hooks';
+import { gamePuzzleId } from './recoil';
 
 type OuterLetters = [string, string, string, string, string, string];
 
@@ -12,16 +12,15 @@ export const useLoadLetters = () => {
   const setPuzzleId = useSetRecoilState(gamePuzzleId);
 
   useEffect(() => {
-    console.log(import.meta.env.BASE_URL);
     gzipJsonFetch(`${import.meta.env.BASE_URL}/words/options.json.gz`)
       .then((options) => options[gameHash % options.length])
       .then((option: string) => {
-        const letters = option.split("");
+        const letters = option.split('');
         const centerLetter = letters[gameHash % letters.length];
         const outerLetters = letters.filter(
-          (letter) => letter !== centerLetter
+          (letter) => letter !== centerLetter,
         ) as OuterLetters;
-        return [centerLetter, ...outerLetters.sort()].join("");
+        return [centerLetter, ...outerLetters.sort()].join('');
       })
       .then((puzzleId) => setPuzzleId(puzzleId))
       .catch((e) => {
--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,27 @@
-import pako from "pako";
+import pako from 'pako';
 
 export const jsonFetch = (url: string) =>
-  fetch(url, { headers: { "Content-Type": "application/json" } }).then((resp) =>
-    resp.json()
+  fetch(url, { headers: { 'Content-Type': 'application/json' } }).then((resp) =>
+    resp.json(),
   );
 
 export const gzipJsonFetch = async (url: string) => {
-  const resp = await fetch(url.replace(/^\/\//, "/"), {
-    headers: { "Content-Type": "application/json", "Content-Encoding": "gzip" },
+  const resp = await fetch(url.replace(/^\/\//, '/'), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Encoding': 'gzip',
+    },
   });
-  try {
+
+  // I don't know why this behaves differently than on production.
+  if (import.meta.env.DEV) {
     return resp.json();
-  } catch (ex) {
-    console.warn("Could not parse as JSON", ex);
-    const buf = await resp.arrayBuffer();
-    const buf_1 = new Uint8Array(buf);
-    const buf_2 = pako.inflate(buf_1, { raw: true });
-    const str = new TextDecoder("utf-8").decode(buf_2);
-    return JSON.parse(str);
   }
+
+  const buf = await resp
+    .arrayBuffer()
+    .then((buf) => new Uint8Array(buf))
+    .then((buf) => pako.inflate(buf));
+  const str = new TextDecoder('utf-8').decode(buf);
+  return JSON.parse(str);
 };
