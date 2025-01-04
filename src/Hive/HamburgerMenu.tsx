bc82703b05cc43700bdb68ce829b6da51ec31807
Evan Charlton <evancharlton@gmail.com>
2025-01-04 11:20:30 +0100

    feat: Adopt spa-components/ShareDialog

--- src/Hive/HamburgerMenu.tsx
+++ src/Hive/HamburgerMenu.tsx
@@ -9,9 +9,22 @@ import {
   HamburgerMenu as SpaHamburgerMenu,
 } from '../spa-components/HamburgerMenu';
 import { useDialog, useShowDialog } from '../dialogs';
+import { ShareDialog } from '../spa-components/ShareDialog';
+import { useHref, useParams } from 'react-router';
+import { useGameId } from '../hooks';
+
+const useCurrentUrl = () => {
+  const { lang } = useParams();
+  const { gameId } = useGameId();
+  const href = useHref(`/${lang}/${gameId}`);
+
+  return [`${window.location.protocol}/`, window.location.host, href].join('/');
+};
 
 export const HamburgerMenu = () => {
   const { open, hide, show } = useDialog('hamburger');
+  const share = useDialog('share');
+
   const showDialog = useShowDialog();
 
   return (
@@ -20,6 +33,12 @@ export const HamburgerMenu = () => {
         icon={MdLink}
         text="Del puslespill"
         onClick={() => showDialog('share')}
+      />
+      <ShareDialog
+        url={useCurrentUrl()}
+        shareText="Prøve å finne alle ord i idags bikuben!"
+        open={share.open}
+        onClose={() => share.hide()}
       />
       <div style={{ flex: 1, borderTop: '1px solid #999a' }}></div>
       <Action
