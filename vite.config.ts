bdab1561e5c10ddcc4c8043e307cb55aad20abc7
Evan Charlton <evancharlton@gmail.com>
2025-01-15 18:15:28 +0100

    fix: Prefer cached JSON files
    Instead of preferring the network, use the cached files if possible.
    These don't change very often (and aren't expected to in the future).

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,44 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      base: '/',
+      base: "/",
       manifest: {
-        name: 'Stavehumle',
-        short_name: 'Stavehumle',
-        theme_color: '#ffd413',
+        name: "Stavehumle",
+        short_name: "Stavehumle",
+        theme_color: "#ffd413",
         icons: [
           {
-            src: '/logo-64.png',
-            sizes: '64x64 32x32',
-            type: 'image/png',
+            src: "/logo-64.png",
+            sizes: "64x64 32x32",
+            type: "image/png",
           },
           {
-            src: '/logo-192.png',
-            type: 'image/png',
-            sizes: '192x192',
+            src: "/logo-192.png",
+            type: "image/png",
+            sizes: "192x192",
           },
           {
-            src: '/logo-512.png',
-            type: 'image/png',
-            sizes: '512x512',
+            src: "/logo-512.png",
+            type: "image/png",
+            sizes: "512x512",
           },
         ],
-        start_url: '.',
+        start_url: ".",
       },
 
-      devOptions: { enabled: true, navigateFallback: 'index.html' },
+      devOptions: { enabled: true, navigateFallback: "index.html" },
       workbox: {
-        cacheId: 'stavehumle',
+        cacheId: "stavehumle",
         runtimeCaching: [
           {
             urlPattern: /\.json$/,
-            handler: 'NetworkFirst',
+            handler: "StaleWhileRevalidate",
           },
         ],
       },
